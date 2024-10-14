888101c49b0d2a7f7fe019ebcaf63e8b076b2f65
_Dedushka_ <zomb689@gmail.com>
2024-10-14 20:17:36 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cin;
+using std::cout;
+
+std::vector<std::string> sha1(const std::string& message){
+    const size_t lenght = message.size() * 8;
+
+}
+
+int main(){
+    int A,B,C,D,E;
+    A=0x67452301;
+    B=0xEFCDAB89;
+    C=0x98BADCFE;
+    D=0x10325476;
+    E=0xC3D2E1F0;
+    return 0;
+}
